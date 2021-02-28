5e699afc9f29e39e273c71918390becd33438113
yashgaherwar <72980929+yashgaherwar@users.noreply.github.com>
2021-02-28 20:00:20 +0530

    Create even_and_odd_using_if_else.c

---
+++ even_and_odd_using_if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter a number",a);
+   scanf("%d",&a);
+   if(a%2==0)
+   {
+       printf("number is even%d",a);
+       
+   }
+   else
+   {
+       printf("number is odd%d");
+   }
+    
+    return 0;
+}
