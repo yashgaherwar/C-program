416507a7042cbcc6407a05619342f28bd93eaaae
yashgaherwar <72980929+yashgaherwar@users.noreply.github.com>
2021-02-28 19:41:23 +0530

    Create range_of_datatypes.c

---
+++ range_of_datatypes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+
+int main() 
+{
+    printf("int min=%d\n",INT_MIN);
+     printf("int max=%d\n",INT_MAX);
+      printf("float min =%e\n",FLT_MIN);
+       printf("float max=%e\n",FLT_MAX);
+         printf("double min=%e\n",DBL_MIN);
+           printf("double max=%e\n",DBL_MAX);
+             printf("char min =%d\n",CHAR_MIN);
+              printf("char max =%d\n",CHAR_MAX);
+              
+               printf("float precision =%d\n",FLT_DIG);
+              
+     return 0;
+}
