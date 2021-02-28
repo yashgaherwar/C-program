a8c431ff4bda25ee34f6f274071079ae52b8f5af
yashgaherwar <72980929+yashgaherwar@users.noreply.github.com>
2021-02-28 20:06:09 +0530

    Create print_ascii_value.c

---
+++ print_ascii_value.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+    int main()
+    {
+       char chr1,chr2;
+       chr1 = 101;
+       chr2='e';
+       printf("%c\n", chr1);
+       printf("%d\n", chr1);
+       
+       printf("%c\n", chr2);
+       printf("%d\n", chr2);
+       return 0;
+    }
